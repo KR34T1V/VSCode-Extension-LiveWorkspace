f48437c89f3f38f1d936d9c20dabe71d8c691036
m4dm0nk3y <m4dm0nk3y.za@gmail.com>
2019-04-05 15:39:04 +0200

    moved some stuff

--- src/extension.ts
+++ src/extension.ts
@@ -4,16 +4,6 @@ import * as name from './constants';
 import * as fs from 'fs';
 import * as ftp from './FTP/FTP_Functions';
 import * as i from './interfaces';
-import { OutgoingMessage } from 'http';
-
-export function manageProtocol(settings: i.SettingsJSON){
-    if (settings.protocol === "sftp"){
-        //CALL SFTP PATH 
-    }
-    else {
-        //CALL FTP PATH
-    }
-}
 
 export function activate(context: vscode.ExtensionContext) {
 		console.log('Congratulations, your extension "ftp-filecontrol" is now active!');
