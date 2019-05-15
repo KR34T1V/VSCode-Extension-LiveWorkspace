c57ea681aee408353b8d572fad3864e3e016d6ad
m4dm0nk3y <m4dm0nk3y.za@gmail.com>
2019-05-15 09:56:55 +0200

    opening live view will initialize extension

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,8 @@ import * as vscode from 'vscode';
 import { FtpExplorer, checkOutFile, checkInFile, uploadFile, viewFile, downloadFile, renameFile, deleteFile, createNewFolder, createNewFile, refreshTree } from './commands/';
 
 export function activate(context: vscode.ExtensionContext) {
-		console.log('Congratulations, your extension "ftp-filecontrol" is now active!');
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-        /*Create Folder*/
-        new FtpExplorer(context);
-    });
-	context.subscriptions.push(disposable);
+    console.log('Congratulations, your extension "ftp-filecontrol" is now active!');
+    new FtpExplorer(context);
 	context.subscriptions.push(vscode.commands.registerCommand('live-workspace.checkout', node => checkOutFile(node)));
 	context.subscriptions.push(vscode.commands.registerCommand('live-workspace.checkin', node => checkInFile(node)));
     context.subscriptions.push(vscode.commands.registerCommand('live-workspace.upload', node => uploadFile(node)));
