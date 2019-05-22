ab61bdc811c96c9ec4b18261eff87f6ac47c6bc9
m4dm0nk3y <m4dm0nk3y.za@gmail.com>
2019-05-22 14:14:11 +0200

    rearranged the extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -7,17 +7,17 @@ export function activate(context: vscode.ExtensionContext) {
     console.log(`Congratulations, your extension ${EXTENSION_NAME} is now active!`);
     VSCODE_OUTPUT.appendLine(`${EXTENSION_NAME} is now active!`)
     context.subscriptions.push(vscode.commands.registerCommand('live-workspace.setup', ()=>setupWorkspace()));
-        new FtpExplorer(context);
-        context.subscriptions.push(vscode.commands.registerCommand('live-workspace.checkout', node => checkOutFile(node)));
-        context.subscriptions.push(vscode.commands.registerCommand('live-workspace.checkin', node => checkInFile(node)));
-        context.subscriptions.push(vscode.commands.registerCommand('live-workspace.upload', node => uploadFile(node)));
-        context.subscriptions.push(vscode.commands.registerCommand('live-workspace.download', node => downloadFile(node)));
-        context.subscriptions.push(vscode.commands.registerCommand('live-workspace.rename', node => renameFile(node)));
-        context.subscriptions.push(vscode.commands.registerCommand('live-workspace.delete', node => deleteFile(node)));
-        context.subscriptions.push(vscode.commands.registerCommand('live-workspace.newfolder', node => createNewFolder(node)));
-        context.subscriptions.push(vscode.commands.registerCommand('live-workspace.newfile', node => createNewFile(node)));
-        vscode.commands.registerCommand('live-workspace.openFtpResource', resource => viewFile(resource));
-        vscode.commands.executeCommand('setContext', EXTENSION_NAME +"."+"enabled", true);
+    context.subscriptions.push(vscode.commands.registerCommand('live-workspace.checkout', node => checkOutFile(node)));
+    context.subscriptions.push(vscode.commands.registerCommand('live-workspace.checkin', node => checkInFile(node)));
+    context.subscriptions.push(vscode.commands.registerCommand('live-workspace.upload', node => uploadFile(node)));
+    context.subscriptions.push(vscode.commands.registerCommand('live-workspace.download', node => downloadFile(node)));
+    context.subscriptions.push(vscode.commands.registerCommand('live-workspace.rename', node => renameFile(node)));
+    context.subscriptions.push(vscode.commands.registerCommand('live-workspace.delete', node => deleteFile(node)));
+    context.subscriptions.push(vscode.commands.registerCommand('live-workspace.newfolder', node => createNewFolder(node)));
+    context.subscriptions.push(vscode.commands.registerCommand('live-workspace.newfile', node => createNewFile(node)));
+    vscode.commands.registerCommand('live-workspace.openFtpResource', resource => viewFile(resource));
+    vscode.commands.executeCommand('setContext', EXTENSION_NAME +"."+"enabled", true);
+    new FtpExplorer(context);
 }
 
 export function deactivate() {}
