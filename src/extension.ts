0a1010a0cc096aef74f8bf6d2e93f10a05b4838f
m4dm0nk3y <m4dm0nk3y.za@gmail.com>
2019-05-22 11:14:44 +0200

    changed a few things

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,12 @@
 import * as vscode from 'vscode';
 import { FtpExplorer, checkOutFile, checkInFile, uploadFile, viewFile, downloadFile, renameFile, deleteFile, createNewFolder, createNewFile, refreshTree, setupWorkspace } from './commands/';
-import { EXTENSION_NAME } from './constants';
+import { EXTENSION_NAME, VSCODE_OUTPUT } from './constants';
 import { localCreateSettings, localCreateSettingsFolder, localExistSettingsFolder, localExistSettings } from './fileExplorer';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "ftp-filecontrol" is now active!');
+    console.log(`Congratulations, your extension ${EXTENSION_NAME} is now active!`);
+    VSCODE_OUTPUT.appendLine(`${EXTENSION_NAME} is now active!`)
+    context.subscriptions.push(vscode.commands.registerCommand('live-workspace.setup', ()=>setupWorkspace()));
         new FtpExplorer(context);
         context.subscriptions.push(vscode.commands.registerCommand('live-workspace.checkout', node => checkOutFile(node)));
         context.subscriptions.push(vscode.commands.registerCommand('live-workspace.checkin', node => checkInFile(node)));
@@ -14,8 +16,6 @@ export function activate(context: vscode.ExtensionContext) {
         context.subscriptions.push(vscode.commands.registerCommand('live-workspace.delete', node => deleteFile(node)));
         context.subscriptions.push(vscode.commands.registerCommand('live-workspace.newfolder', node => createNewFolder(node)));
         context.subscriptions.push(vscode.commands.registerCommand('live-workspace.newfile', node => createNewFile(node)));
-        context.subscriptions.push(vscode.commands.registerCommand('live-workspace.setup', ()=>setupWorkspace()));
-        //context.subscriptions.push(vscode.commands.registerCommand('live-workspace.refresh', () => refreshTree(context)));
         vscode.commands.registerCommand('live-workspace.openFtpResource', resource => viewFile(resource));
         vscode.commands.executeCommand('setContext', EXTENSION_NAME +"."+"enabled", true);
 }
